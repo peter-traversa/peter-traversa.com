fef4ddd3ad922c61c7346f8c589db07455e22038
github username <github email address>
2018-11-28 18:05:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h2>Peter Traversa's Portfolio Site</h2>
           <a
             className="App-link"
             href="https://reactjs.org"
