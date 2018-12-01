f18547b6fb798bb600156fa1658f5ac5ef8e9279
github username <github email address>
2018-12-01 12:57:18 -0500

    placeholder site commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Github from './Components/GitHub';
+// import Github from './Components/GitHub';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>Peter Traversa's Portfolio Site</h2>
+          <h2>Portfolio Placeholder</h2>
         </header>
-        <Github />
+        {/* <Github /> */}
       </div>
     );
   }
