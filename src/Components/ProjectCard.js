bb8b2557500372ec3af21563312b77e89140d6f4
github username <github email address>
2018-12-02 15:16:38 -0500

    semantic ui css added to index.html, cards working

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
+import '../Stylesheets/ProjectCard.css'
 
 export default function ProjectCard(props) {
   // debugger
   return (
-    <Card>
-      <Image src='../../public/favicon.ico'/>
+    <Card color='blue' raised >
+      <Image src='favicon.ico'/>
       <Card.Content>
         <Card.Header>{props.project.name}</Card.Header>
       </Card.Content>
--- src/Components/ProjectsPage.js
+++ src/Components/ProjectsPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ProjectCard from './ProjectCard';
 import { Card } from 'semantic-ui-react';
+import '../Stylesheets/ProjectsPage.css';
 
 export default class ProjectsPage extends Component {
   constructor() {
@@ -19,7 +20,7 @@ export default class ProjectsPage extends Component {
 
   render() {
     return (
-      <Card.Group>
+      <Card.Group className='project-cards'>
         {this.state.projects.map((project, idx) => {
           return (
             <ProjectCard key={idx} project={project}/>
