c43141e1fc552d9c091084ff0e9b1cc266fc5d85
github username <github email address>
2018-12-02 16:57:02 -0500

    starting react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './Stylesheets/App.css';
 import ProjectsPage from './Components/ProjectsPage';
-import BioPage from './Components/BioPage'
+import BioPage from './Components/BioPage';
 import HomePage from './Components/HomePage';
+import ResumePage from './Components/ResumePage';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <HomePage />
         <ProjectsPage />
         <BioPage />
+        <ResumePage />
       </React.Fragment>
     );
   }
---
+++ src/Components/ResumePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ResumePage() {
+  return <h1>Resume Page Placeholder</h1>
+};
